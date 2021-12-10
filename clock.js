ed40e5e5e419ee2bd5e762ff1c2f3c44fda6fb42
DevTechJr <devtechjr@gmail.com>
2021-12-09 20:15:41 -0500

    new credit feature :)

--- clock.js
+++ clock.js
@@ -1,14 +1,20 @@
 const clock = document.querySelector(".clock");
 clockDate = document.querySelector(".clock-date");
 clockTime = document.querySelector(".clock-time");
-
+const credit = document.querySelector(".credit");
+const builtDate = new Date("December 09, 21 18:55");
+timeBuilt = Math.round(1639054500000 / 1000 / 60 / 60);
 const updateClock = () => {
   const now = new Date();
   const h = now.getHours();
   const m = now.getMinutes();
   const s = now.getSeconds();
+  let timeStampRN = now.getTime();
+  timeStampRN = Math.round(timeStampRN / 1000 / 60 / 60);
+  credit.innerHTML = `<h2 class="credit">Built by Anirudh Bharadwaj Vangara,${
+    timeStampRN - timeBuilt
+  }</h2> minutes ago </h2>`;
 
-  console.log(now.toDateString());
   clockDate.textContent = now.toDateString();
 
   clockTime.innerHTML = `<div class="clock-time-hours">${h} :</div>
